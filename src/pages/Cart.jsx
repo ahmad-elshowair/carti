e295c39f2a31b1fdc57ebebc8c6fb96a7510d730
Ahmad Elshowair <it.is.work@outlook.com>
2022-07-20 12:15:28 +0200

    style the added products to the cart

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -4,59 +4,54 @@ const Cart = ({cart, setCart}) => {
 
   const allProducts= cart.map((product) =>{
     return(
-      <article key={product.id} className='product'>
-        <div className='product-details'>
-          <div className='product-details_image'>
-            <img className='product-image' src={product.image} alt={product.name}/>
-          </div>
-          <div className='product-details_info'>
-            <h3 className='product-details_info_name'>
-              {product.name}
-            </h3>
-            <p className='product-details_info_description'>
-              {product.description}
-            </p>
-            <span className='product-details_info_price'>
-              {product.price}
-            </span>
+      <article key={product.id} className='cart-products_product'>
+        <div className="container">
+          <div className='product-details'>
+            <div className='product-details_image'>
+              <img className='details-image' src={product.image} alt={product.name}/>
+            </div>
+            <div className='product-details_info'>
+              <h3 className='details-info_name'>
+                {product.name}
+              </h3>
+              <p className='details-info_description'>
+                {product.description}
+              </p>
+              <div className='btn-group'>
+                <span className='details-info_price'>
+                  {product.price}
+                </span>
+                <span 
+                  className='btn-remove'
+                  onClick={()=>setCart(cart.filter(p=> p.id !== product.id))}
+                  >
+                  Remove
+              </span>
+              </div>
+            </div>
           </div>
         </div>
-        {
-          cart.includes(product) ?(
-            <button 
-              className='btn remove-cart'
-              onClick={()=>setCart(cart.filter(p=> p.id !== product.id))}
-            >
-              Remove
-            </button>
-          ):(
-            <button 
-              className='btn add-cart'
-              onClick={() => setCart([...cart, product])}
-            >
-              Add
-            </button>
-
-            
-          ) 
-        }
       </article>
     )
   })
   return (
     <section className='cart-section'>
-      <div className='container'>
-        <div className='cart-section_products'>
-
-        
-        {allProducts}
-          <article className='cart-info'>
-            <h3 className='cart-info_header'>
-              Total: 
-            </h3>
-            <span>products: {cart.length}</span>
-            <span>price: </span>
-          </article>
+      <div className='cart-section_products'>
+        <article className='cart-info'>
+          <div className="container">
+            <div className='cart-info_box'>
+              <h3 className='cart-info_header'>
+                Total
+              </h3>
+              <div className="cart-info-total">
+                <span className='cart-info_total-products'>products: {cart.length}</span>
+                <span className='cart-info_total-price'>price: </span>
+              </div>
+            </div>
+          </div>
+        </article>
+        <div className='cart-products'>
+          {allProducts}
         </div>
       </div>
     </section>
